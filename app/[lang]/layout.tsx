cdf85fd7ce27d8862405f63614a26d3e9dcc233c
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2026-04-14 23:23:11 +0200

    metaImage

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -14,34 +14,51 @@ const cairo = Cairo({
 
 export const metadata: Metadata = {
   title: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة ",
-  description: "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
-  keywords: ["اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة", "sorooj academy for contemporary intellectual studies and research", "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة", "sorooj academy for contemporary intellectual studies and research", "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة", "sorooj academy for contemporary intellectual studies and research"],
-  authors: [{ name: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة", url: "https://academy.sorooj.org" }],
+  description:
+    "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
+  keywords: [
+    "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
+    "sorooj academy for contemporary intellectual studies and research",
+    "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
+    "sorooj academy for contemporary intellectual studies and research",
+    "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
+    "sorooj academy for contemporary intellectual studies and research",
+  ],
+  authors: [
+    {
+      name: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
+      url: "https://academy.sorooj.org",
+    },
+  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
   },
   openGraph: {
     title: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
-    description: "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
+    description:
+      "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
     url: "https://academy.sorooj.org",
     siteName: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
     locale: "ar",
     type: "website",
     images: [
       {
-        url: "https://academy.sorooj.org/logo.png",
+        url: "https://academy.sorooj.org/assets/images/meta.png",
         alt: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
+        width: 1200,
+        height: 630,
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     title: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
-    description: "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
+    description:
+      "اكاديمية سُرُجْ - منارة للبحث العلمى والتعليم المتخصص فى استكشاف وفهم المذاهب الفكرية المعاصرة , بما فى ذلك الدينية والفلسفية والسياسية من اجل تمكين المتعلمين فى اتخاذ قرارات مستنيرة فى ظل التنوع الفكرى المتزايد",
     images: [
       {
-        url: "https://academy.sorooj.org/logo.png",
+        url: "https://academy.sorooj.org/assets/images/meta.png",
         alt: "اكاديمية سرج للدرسات والابحاث الفكرية المعاصرة",
       },
     ],
